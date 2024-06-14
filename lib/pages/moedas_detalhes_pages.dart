e38d28677687d8137091b4accd94a3f18e1aed39
Débora Rodrigues <81527964+souzadbr@users.noreply.github.com>
2024-06-14 17:09:33 -0300

    formatar formulario na pagina de detalhe moedas

--- lib/pages/moedas_detalhes_pages.dart
+++ lib/pages/moedas_detalhes_pages.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:flutter_aula_1/models/moeda.dart";
+import "package:intl/intl.dart";
 
 class MoedasDetalhesPages extends StatefulWidget {
 
@@ -12,7 +14,10 @@ class MoedasDetalhesPages extends StatefulWidget {
 }
 
 class _MoedasDetalhesPagesState extends State<MoedasDetalhesPages> {
-  
+  NumberFormat real = NumberFormat.currency(locale: "pt_BR", name: "R\$");
+  final _form = GlobalKey<FormState>();
+  final _valor = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,12 +26,54 @@ class _MoedasDetalhesPagesState extends State<MoedasDetalhesPages> {
       ),
       body: Column(
         children: [
-          Row(
-            children: [
-              SizedBox(child: Image.asset(widget.moeda.icone), 
-              width: 50,)
+          Padding(
+            padding: EdgeInsets.only(bottom: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  child: Image.asset(widget.moeda.icone), 
+                  width: 50,
+                ),
+                Container(width: 10),
+                Text(
+                  real.format(widget.moeda.preco),
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: -1,
+                    color: Colors.grey[800],
+                  ),
+                )
             ],
+          )
           ),
+          Form(
+          key: _form,
+          child: TextFormField(
+            controller: _valor,
+            style: TextStyle (fontSize: 22),
+            decoration: InputDecoration(
+             border: OutlineInputBorder(),
+             labelText: "Valor",
+             prefixIcon: Icon(Icons.monetization_on_outlined), 
+             suffix: Text(
+              "reais",
+              style: TextStyle(fontSize: 14),
+             )
+            ),
+            keyboardType: TextInputType.number,
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            validator: (value) {
+              if(value.isEmpty) {
+                return "Informe valor da compra";
+              } else if (double.parse(value) < 50) {
+                return "Compra minina é de R\$ 50 reais";
+              }
+            },
+            )
+          )
         ],
       ),
     );
